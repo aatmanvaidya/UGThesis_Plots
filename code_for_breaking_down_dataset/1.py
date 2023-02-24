cc28ca5a0237933ceb34eae098de346bb5ff4f9c
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-02-24 12:55:17 +0530

    changes

--- code_for_breaking_down_dataset/1.py
+++ code_for_breaking_down_dataset/1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("D:\\Study\\UG Thesis\\Datasets\\breakingDatasetForNewConversion\\tweets_retweetsFile.csv", engine='python') 
-# print(df.shape)
-dfs = np.array_split(df, 10)
+df = pd.read_csv("D:\\Study\\UG Thesis\\Code\\code_for_breaking_down_dataset\\temp.csv") 
+print(df.shape)
+dfs = np.array_split(df, 3)
 
 for index, df in enumerate(dfs):
     df.to_csv(f'tweets_retweetsFile_Part{index+1}.csv',index = False)
